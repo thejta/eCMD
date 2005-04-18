430a233706de26744c45789747e4c3db352f8504
Paul Prahl <prahl@us.ibm.com>
2005-04-18 07:32:43 -0500

    STGC: Clean up Beam messages.

--- capi/ecmdSharedUtils.C
+++ capi/ecmdSharedUtils.C
@@ -19,6 +19,14 @@
 //
 // End Module Description **********************************************
 
+// Change Log *********************************************************
+//                                                                      
+//  Flag Reason   Vers Date     Coder     Description                       
+//  ---- -------- ---- -------- -----     -----------------------------
+//  @01  STGC7449      04/18/05 prahl     Fix up Beam messages.
+//   
+// End Change Log *****************************************************
+
 //----------------------------------------------------------------------
 //  Includes
 //----------------------------------------------------------------------
@@ -240,19 +248,20 @@ uint32_t ecmdHashString32(const char *k, uint32_t c)
     }
     /*------------------------------------- handle the last 11 bytes */
     c += length;
+    // Add /*fall through*/ to tell Beam to ignore and not throw an error. @01a
     switch(len)  /* all the case statements fall through */
     {
-    case 11: c+=((uint32_t)k[10]<<24);
-    case 10: c+=((uint32_t)k[9]<<16);
+    case 11: c+=((uint32_t)k[10]<<24); /*fall through*/
+    case 10: c+=((uint32_t)k[9]<<16);  /*fall through*/
     case 9 : c+=((uint32_t)k[8]<<8);
-        /* the first byte of c is reserved for the length */
-    case 8 : b+=((uint32_t)k[7]<<24);
-    case 7 : b+=((uint32_t)k[6]<<16);
-    case 6 : b+=((uint32_t)k[5]<<8);
-    case 5 : b+=k[4];
-    case 4 : a+=((uint32_t)k[3]<<24);
-    case 3 : a+=((uint32_t)k[2]<<16);
-    case 2 : a+=((uint32_t)k[1]<<8);
+        /* the first byte of c is reserved for the length */ /*fall through*/
+    case 8 : b+=((uint32_t)k[7]<<24);  /*fall through*/
+    case 7 : b+=((uint32_t)k[6]<<16);  /*fall through*/
+    case 6 : b+=((uint32_t)k[5]<<8);   /*fall through*/
+    case 5 : b+=k[4];                  /*fall through*/
+    case 4 : a+=((uint32_t)k[3]<<24);  /*fall through*/
+    case 3 : a+=((uint32_t)k[2]<<16);  /*fall through*/
+    case 2 : a+=((uint32_t)k[1]<<8);   /*fall through*/
     case 1 : a+=k[0];
         /* case 0: nothing left to add */
     }
