ace694ff7d9cd926aa54e75439acc793c4c36941
Chris Engel <cjengel@us.ibm.com>
2005-04-18 13:59:03 -0500

    More v5.0 changes

--- capi/ecmdSharedUtils.C
+++ capi/ecmdSharedUtils.C
@@ -390,3 +390,67 @@ std::string ecmdWriteTarget (ecmdChipTarget & i_target, ecmdTargetDisplayMode_t 
   return printed;
 
 }
+
+
+uint32_t ecmdGenB32FromHex (uint32_t * o_numPtr, const char * i_hexChars, int startPos) {
+
+  uint32_t tempB32 = 0;
+  uint32_t counter = 0;
+  char	twoChars[2] = "0";
+
+  if ((o_numPtr == NULL) || (i_hexChars == NULL) || (strlen(i_hexChars) == 0))
+    return 0xFFFFFFFF;
+
+  o_numPtr[0] = 0x0;
+  o_numPtr[startPos>>3]= 0x0;
+
+  for (counter = 0; counter < strlen(i_hexChars); counter++) {
+    if (((counter + startPos) & 0xFFF8) == (counter + startPos))
+      o_numPtr[(counter+startPos)>>3] = 0x0;
+    twoChars[0] = i_hexChars[counter];
+    tempB32 = strtoul((char *) & twoChars[0], NULL, 16);
+    o_numPtr[(counter+startPos)>>3] |= (tempB32 << (28 - (4 * ((counter + startPos) & 0x07))));
+  }
+
+  return o_numPtr[0];
+
+}
+
+
+uint32_t ecmdGenB32FromHexLeft (uint32_t * o_numPtr, const char * i_hexChars) {
+
+  if ((i_hexChars == NULL) || (o_numPtr == NULL))
+    return 0xFFFFFFFF;
+
+  return ecmdGenB32FromHex(o_numPtr, i_hexChars, 0);
+
+}
+
+uint32_t ecmdGenB32FromHexRight (uint32_t * o_numPtr, const char * i_hexChars, int expectBits) {
+
+  int stringSize;
+
+  if ((i_hexChars == NULL) || (o_numPtr == NULL))
+    return 0xFFFFFFFF;
+
+  /* ----------------------------------------------------------------- */	
+  /* Telling us we are expecting few bits than the user input          */	
+  /* ----------------------------------------------------------------- */	
+  stringSize = strlen(i_hexChars);
+
+  if (expectBits==0) 
+    expectBits = ((stringSize << 2) + 31) & 0xFFFE0;
+
+  /* ----------------------------------------------------------------- */
+  /* Can't figure out if this case is bad...                           */
+  /* ----------------------------------------------------------------- */
+  if (expectBits<(stringSize*4)) 
+    expectBits = (4-(expectBits & 0x3))+expectBits+(stringSize*4-expectBits&0xFFFC);
+
+  expectBits = (expectBits - stringSize*4)>>2; 
+
+  return ecmdGenB32FromHex(o_numPtr, i_hexChars, expectBits );
+
+
+}
+
