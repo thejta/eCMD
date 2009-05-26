694f04ad1ad91ed0f298c5bcc11d0726a7e154da
Jason Albert <albertj@us.ibm.com>
2009-05-26 14:58:41 -0500

    Updates to build perl documentation in a more perl like fashion

--- docs/createDocumentation.pl
+++ docs/createDocumentation.pl
@@ -69,6 +69,8 @@ system("mkdir -p $outputDirectory/Perlapi");
 # Copy over the header files we are going to process
 $rc = system("cp $cvsBase/perlapi/ecmdBit64.H $outputDirectory/Perlapi/.");
 if ($rc) { return $rc; }
+$rc = system("cp $cvsBase/perlapi/ecmdPerlApiTypes.H $outputDirectory/Perlapi/.");
+if ($rc) { return $rc; }
 $rc = system("cp $cvsBase/capi/ecmdDataBuffer.H $outputDirectory/Perlapi/.");
 if ($rc) { return $rc; }
 $rc = system("cp $cvsBase/capi/ecmdStructs.H $outputDirectory/Perlapi/.");
@@ -96,6 +98,50 @@ for (my $x = 0; $x <= $#extensions; $x++) {
   # in the comments that is different from the actual file
   $rc = system("cat $cvsBase/ext/$extensions[$x]/perlapi/$extensions[$x]ClientPerlapi.H $cvsBase/ext/$extensions[$x]/perlapi/$extensions[$x]ClientPerlapiFunc.H | grep -v $extensions[$x]ClientPerlapiFunc.H > $outputDirectory/Perlapi/$extensions[$x]ClientPerlapi.H");
   if ($rc) { return $rc; }
+}
+
+# Open up all the .H files and modify source to change the C variables to perl variables
+my @files = split(/\s+/,`ls $outputDirectory/Perlapi/`);
+for (my $x = 0; $x <= $#files; $x++) {
+  if (-e "$outputDirectory/Perlapi/$files[$x]") {
+    open (INFILE, "<$outputDirectory/Perlapi/$files[$x]") or die "Couldn't open the file $files[$x] for input\n";
+    my @fileLines = <INFILE>;
+    my $fileLine;
+    close INFILE;
+
+    open (OUTFILE, ">$outputDirectory/Perlapi/$files[$x]") or die "Couldn't open the file $files[$x] for output\n";
+
+    foreach $fileLine (@fileLines) {
+
+      # std::list<ecmdXXX> to ecmdXXXList
+      $fileLine =~ s/std::list\s*<\s*([^\s]*?)\s*>/$1List/g;
+
+      # std::vector<ecmdXXX> to ecmdXXXVector
+      $fileLine =~ s/std::vector\s*<\s*([^\s]*?)\s*>/$1Vector/g;
+
+      # Turn a uint32_t reference into a scalarref
+      $fileLine =~ s/uint32_t\s*&/scalarref /g;
+
+      # Turn a char** reference into an arrayref
+      $fileLine =~ s/char\s*\*\*/arrayref /g;
+
+      #const char * to scalar
+      $fileLine =~ s/const char\s*\*/scalar /g;
+
+      #char * to scalar
+      $fileLine =~ s/char\s*\*/scalar/g;
+
+      # vars to convert to scalar
+      $fileLine =~ s/(const\s*)?(uint32_t|uint16_t|uint8_t|bool|std::string)/scalar/g;
+
+      # uint64_t to scalar
+      $fileLine =~ s/uint64_t/ecmdBit64/g;
+
+      print OUTFILE $fileLine;
+    }
+
+    close OUTFILE;
+  }
 }
 
 # Update the version strings
