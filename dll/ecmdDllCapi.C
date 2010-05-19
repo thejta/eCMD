4b651f39e70c915b7e24ee04a2516aaf8588f43d
Michael Baiocchi <baiocchi@us.ibm.com>
2010-05-19 07:57:23 -0500

    Removed PFD-specific header/copyright

--- dll/ecmdDllCapi.C
+++ dll/ecmdDllCapi.C
@@ -1,20 +1,3 @@
-// IBM_PROLOG_BEGIN_TAG 
-// This is an automatically generated prolog. 
-//  
-// fips730 src/ecmd/fsp/ecmdDllCapi.C 1.20.1.16 
-//  
-// IBM CONFIDENTIAL 
-//  
-// OBJECT CODE ONLY SOURCE MATERIALS 
-//  
-// COPYRIGHT International Business Machines Corp. 2004,2010 
-// All Rights Reserved 
-//  
-// The source code for this program is not published or otherwise 
-// divested of its trade secrets, irrespective of what has been 
-// deposited with the U.S. Copyright Office. 
-//  
-// IBM_PROLOG_END_TAG 
 /* $Header$ */
 // Copyright ***********************************************************
 //                                                                      
