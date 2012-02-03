bafeef581ef94b8b5dcd771e82cf212efd4999c0
John Farrugia <farrugia@us.ibm.com>
2012-02-03 16:22:09 -0600

    Set fapi TargetType when converting from eCMD target to fapi target

--- ext/fapi/capi/fapiSharedUtils.C
+++ ext/fapi/capi/fapiSharedUtils.C
@@ -29,7 +29,9 @@
 #include <stdio.h>
 #include <fstream>
 
+#include <fapiTarget.H>
 #include <fapiSharedUtils.H>
+#include <ecmdSharedUtils.H>
 #include <ecmdStructs.H>
 #include <ecmdReturnCodes.H>
 
@@ -41,4 +43,62 @@ void fapiTargetToEcmdTarget(fapi::Target i_fapiTarget, ecmdChipTarget & o_ecmdTa
 
 void ecmdTargetToFapiTarget(ecmdChipTarget & i_ecmdTarget, fapi::Target & o_fapiTarget){
   o_fapiTarget.set(reinterpret_cast<void *> (&i_ecmdTarget));
+  o_fapiTarget.setType(fapi::TARGET_TYPE_NONE);
+
+  /* ChipUnit is the lowest level so lets check it first */
+  if (i_ecmdTarget.chipUnitTypeState == ECMD_TARGET_FIELD_VALID)
+  {
+      if (i_ecmdTarget.chipUnitType == "ex")
+      {
+         o_fapiTarget.setType(fapi::TARGET_TYPE_EX_CHIPLET);
+      } 
+      else if (i_ecmdTarget.chipUnitType == "mcs") 
+      {
+         o_fapiTarget.setType(fapi::TARGET_TYPE_MCS_CHIPLET);
+      } 
+      else if (i_ecmdTarget.chipUnitType == "mba") 
+      {
+         o_fapiTarget.setType(fapi::TARGET_TYPE_MBA_CHIPLET);
+      } 
+      else if (i_ecmdTarget.chipUnitType == "mbs") 
+      {
+         o_fapiTarget.setType(fapi::TARGET_TYPE_MBS_CHIPLET);
+      } 
+      // Need TargetTypes for the follow chipUnits 
+      else if (i_ecmdTarget.chipUnitType == "xbus") 
+      {
+         //o_fapiTarget.setType(fapi::TARGET_TYPE_MBS_CHIPLET);
+      } 
+      else if (i_ecmdTarget.chipUnitType == "abus") 
+      {
+         //o_fapiTarget.setType(fapi::TARGET_TYPE_MBS_CHIPLET);
+      } 
+      else if (i_ecmdTarget.chipUnitType == "occ") 
+      {
+         //o_fapiTarget.setType(fapi::TARGET_TYPE_MBS_CHIPLET);
+      } 
+      else 
+      {
+         // do nothing for now.  Maybe we should error out?
+      }
+  }
+  else if (i_ecmdTarget.chipTypeState == ECMD_TARGET_FIELD_VALID)
+  {
+      if (i_ecmdTarget.chipType == "p8") 
+      {
+         o_fapiTarget.setType(fapi::TARGET_TYPE_PROC_CHIP);
+      } 
+      else if (i_ecmdTarget.chipType == "s1") 
+      {
+         o_fapiTarget.setType(fapi::TARGET_TYPE_PROC_CHIP);
+      } 
+      else if (i_ecmdTarget.chipType == "centaur") 
+      {
+         o_fapiTarget.setType(fapi::TARGET_TYPE_MEMBUF_CHIP);
+      } 
+  }
+  else if (i_ecmdTarget.cageState == ECMD_TARGET_FIELD_VALID)
+  {
+      o_fapiTarget.setType(fapi::TARGET_TYPE_SYSTEM);
+  }
 }
