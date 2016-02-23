b25cadba50351122f316daa8e323f3b9710cf8a7
Jason Albert <albertj@us.ibm.com>
2016-02-23 16:15:50 -0600

    Updated CIP extension name

--- ecmd-core/ext/cip/cmd/snippet/callInterpreter.C
+++ ecmd-core/ext/cip/cmd/snippet/callInterpreter.C
@@ -1,5 +1,5 @@
 #ifdef ECMD_CIP_EXTENSION_SUPPORT
-  /* Cronus/IP Extension */
+  /* Converged IP Extension */
   if ((io_rc == ECMD_INT_UNKNOWN_COMMAND) && (!strncmp("cip",argv[0],3))) {
     io_rc = cipInitExtension();
     if (io_rc == ECMD_SUCCESS) {
