e56fe4e39db2a6043493542664d320c6127d4741
Jason Albert <albertj@us.ibm.com>
2017-10-31 13:54:33 -0500

    Rework of path insert since del line was causing issues

--- ecmd-core/pyapi/init/__init__.py
+++ ecmd-core/pyapi/init/__init__.py
@@ -1,10 +1,11 @@
 # import the right SWIG module depending on Python version
 from sys import version_info
-import sys, os
+from sys import path as sys_path
+from os import path as os_path
 if version_info[0] >= 3:
-    sys.path.insert(0, os.path.join(os.path.dirname(__file__), "python3"))
+    sys_path.insert(0, os_path.join(os_path.dirname(__file__), "python3"))
     from .python3 import *
 else:
-    sys.path.insert(0, os.path.join(os.path.dirname(__file__), "python2"))
+    sys_path.insert(0, os_path.join(os_path.dirname(__file__), "python2"))
     from .python2 import *
-del sys, os, version_info
+del sys_path, os_path, version_info
