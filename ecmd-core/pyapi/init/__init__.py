2a491b868c655d67eca4843e95338f9fc37deaec
Jason Albert <albertj@us.ibm.com>
2017-10-31 12:40:20 -0500

    Added path of module being loaded to sys.path
    - If path not added, the import would fail with:
      ImportError: No module named _ecmd
      This was because it couldn't find the .so
    
    Signed-off-by: Jason Albert <albertj@us.ibm.com>

--- ecmd-core/pyapi/init/__init__.py
+++ ecmd-core/pyapi/init/__init__.py
@@ -1,7 +1,10 @@
 # import the right SWIG module depending on Python version
 from sys import version_info
+import sys, os
 if version_info[0] >= 3:
+    sys.path.insert(0, os.path.join(os.path.dirname(__file__), "python3"))
     from .python3 import *
 else:
+    sys.path.insert(0, os.path.join(os.path.dirname(__file__), "python2"))
     from .python2 import *
-del version_info
+del sys, os, version_info
